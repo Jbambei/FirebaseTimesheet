5421d3f48cafb4d07fd07d3eb7b6275d516c27d8
Joel Bambei <joelbambei@gmail.com>
2019-04-30 14:53:23 -0400

    syn error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -35,15 +35,15 @@ $("#submit").on("click", function(event) {
     // Code in the logic for storing and retrieving the most recent user.
     // Don't forget to provide initial data to your Firebase database.
     name = $("#name-input").val().trim();
-    role = $("#role-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
     rate = $("#rate-input").val().trim();
-    date = $("#date-input").val().trim();
 
     database.ref().push({
         name: name,
-        role: role,
+        destination: destination,
+        frequency: frequency,
         rate: rate,
-        date: date,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
 
 
@@ -52,32 +52,10 @@ $("#submit").on("click", function(event) {
     $("#destination-input").val().trim()
     $("frequency-input").val(""
     $("#rate-input").val("")
+    
     });
     
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
+    
 
 });
 database.ref().on("child_added", function(snapshot) {
